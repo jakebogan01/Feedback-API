ac891995bc04a3904060de91b85963d56eb13fe7
Jake Bogan <jakebogan01@gmail.com>
2023-05-29 12:53:43 -0400

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,12 @@
+const uniqueValidator = require("mongoose-unique-validator");
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
      {
           email: {
                type: String,
                required: [true, "Please enter a email"],
                unique: true,
-               validate: {
-                    isAsync: true,
-                    validator: function (value, isValid) {
-                         const self = this;
-                         return self.constructor.findOne({ email: value }).exec(function (err, user) {
-                              if (err) {
-                                   throw err;
-                              } else if (user) {
-                                   if (self.id === user.id) {
-                                        // if finding and saving then it's valid even for existing email
-                                        return isValid(true);
-                                   }
-                                   return isValid(false);
-                              } else {
-                                   return isValid(true);
-                              }
-                         });
-                    },
-                    message: "The email address is already taken!",
-               },
           },
           password: {
                type: String,
@@ -38,5 +19,6 @@ const userSchema = mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+contactSchema.plugin(uniqueValidator);
 
 module.exports = User;
